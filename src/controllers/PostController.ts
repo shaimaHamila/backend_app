c9afc06afc4ea57361a8e3159775425ced014367
shaimaHamila <hamilachaima18@gmail.com>
2024-02-18 12:00:07 +0100

    test lint6

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -1,30 +1,26 @@
 import { Request, Response } from "express";
 import { appDataSource } from '../config/Database';
-import { Post } from '../entities/Post';
+import { Post } from "../entities/Post";
 export const getPost = async (req: Request, res: Response) => {
     try {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         const posts = await appDataSource.getRepository(Post).find();
-
 
 
 
 
         res.status(200).json({ succss: true, message: "get post successfully", data: posts });
     } catch (error) {
+
+
+
+
+
+
+
+
+
+
+
 
 
 
