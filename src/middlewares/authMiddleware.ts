e6dab5a2628e2359134fdb032f31ad940b4a329b
shaimaHamila <hamilachaima18@gmail.com>
2024-03-10 13:47:45 +0100

    Fix authentication and admin authorization

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,35 +3,100 @@ import * as jwt from "jsonwebtoken";
 import { Admin } from "../entities/Admin";
 import * as dotenv from "dotenv";
 import { appDataSource } from "../config/Database";
+import { User } from "../entities/User";
+import { Teacher } from "../entities/Teacher";
 dotenv.config();
-export const authentification = async (req: Request, res: Response, next: NextFunction) => {
+// Since they are 3 diffrent tables in the database so we do not need to check the role of the user
+// we can just check it for the admin coz he has diffrent roles
+export const adminAuthentification = async (req: Request, res: Response, next: NextFunction) => {
     const header = req.headers.authorization;
     if (!header) {
         return res.status(401).json({ message: "Unauthorized" });
     }
+
     const token = header.split(" ")[1];
     if (!token) {
         return res.status(401).json({ message: "Unauthorized" });
     }
-    const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY!);
-    if (!decode) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-    (req as any).currentUser = decode;
-    next();
-};
 
-export const authorization = (roles: string[]) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        const userRepo = appDataSource.getRepository(Admin);
-        const admin = await userRepo.findOne({
-            //where: { id: req["currentUser"].id },
-            where: { id: (req as any).currentUser.id },
-        });
-        console.log(admin);
-        if (!roles.includes(admin!.role)) {
-            return res.status(403).json({ message: "Forbidden" });
+    try {
+        const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY!) as jwt.JwtPayload;
+
+        if (!decode || !decode.id) {
+            throw new Error("Invalid token!");
+        }
+        const adminRepository = appDataSource.getRepository(Admin);
+        const admin = await adminRepository.findOne({ where: { id: decode.id } });
+
+        if (!admin) {
+            throw new Error("Admin not found");
+        }
+        // Assign user role to the req object
+        if (admin) {
+            res.locals.userRole = admin.role;
         }
         next();
-    };
+    } catch (error: any) {
+        return res.status(401).json({ error: error.message });
+    }
+};
+
+export const userAuthentification = async (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers.authorization;
+    if (!header) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const token = header.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY!) as jwt.JwtPayload;
+        if (!decode || !decode.id) {
+            throw new Error("Invalid token!");
+        }
+
+        const userRepository = appDataSource.getRepository(User);
+        const user = await userRepository.findOne({ where: { id: decode.id } });
+
+        if (!user) {
+            throw new Error("user not found or invalid token!");
+        }
+
+        next();
+    } catch (error: any) {
+        return res.status(401).json({ message: "Invalid token!" + error.message });
+    }
+};
+
+export const teacherAuthentification = async (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers.authorization;
+    if (!header) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const token = header.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.TOKEN_SECRET_KEY!) as jwt.JwtPayload;
+        if (!decode || !decode.id) {
+            throw new Error("Invalid token!");
+        }
+
+        const teacherRepository = appDataSource.getRepository(Teacher);
+        const teacher = await teacherRepository.findOne({ where: { id: decode.id } });
+
+        if (!teacher) {
+            throw new Error("Teacher not found or invalid token!");
+        }
+
+        next();
+    } catch (error: any) {
+        return res.status(401).json({ message: error.message });
+    }
 };
